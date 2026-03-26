0591eeb805629f2172aec1551a6d1526cf8eee89
Souhail-Bel <robotboy976@gmail.com>
2026-03-26 19:26:15 +0100

    Cleaned up project

--- sql.sql
+++ sql.sql
@@ -1,43 +1,38 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
--- if we gonna verify, we use:
--- SELECT * FROM utilisateur
--- WHERE name = $1 AND password = crypt($2, password);
--- where $1 is the guess for username and $2 for password
+CREATE TYPE  user_role AS  ENUM ('normal','admin');
 
--- CREATE TYPE  role AS  ENUM ('normal','admin');
-
-CREATE TABLE IF NOT EXISTS utilisateur(
+CREATE TABLE IF NOT EXISTS utilisateur (
     id SERIAL,
-    name VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL UNIQUE,
     --5allehe 3allah
     password TEXT NOT NULL,
-    role role,
+    role user_role DEFAULT 'normal',
     CONSTRAINT pk_user PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS section(
+CREATE TABLE IF NOT EXISTS section (
     id SERIAL PRIMARY KEY,
-    des TEXT
+    designation VARCHAR(10) NOT NULL,
+    description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS etudiant(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    date_de_naiss DATE NOT NULL,
-    img VARCHAR(256) NOT NULL,
-    section INTEGER,
-    CONSTRAINT fk_section FOREIGN KEY(section) REFERENCES SECTION(id)
+    date_de_naissance DATE NOT NULL,
+    img_url VARCHAR(256) NOT NULL,
+    section_id INTEGER REFERENCES section(id) ON DELETE SET NULL
 );
 
 
--- INSERT INTO  utilisateur(name,password,role) VALUES
--- ('talel zighni', crypt('123', gen_salt('bf')), 'admin'),
--- ('ahmed el hai', crypt('1234', gen_salt('bf')), 'normal');
+INSERT INTO section (designation, description) VALUES
+('GL', 'Genie Logiciel'),
+('RT', 'Resaux et Telecommunications'),
+('IIA', 'Informatique Industrielle et Automatique'),
+('IMI', 'Instrumentation et Maintenance Industrielle');
 
-
--- Un user normal n’a le droit qu’à la consultation
--- 7. Vous allez avoir une table etudiant avec un nom, une
--- image, une date de naissance et une section
--- 8. La table section devra avoir une designation et une
--- description.
+INSERT INTO  utilisateur(username, password, role) VALUES
+('talel zighni', crypt('123', gen_salt('bf')), 'admin'),
+('ahmed el hani', crypt('1234', gen_salt('bf')), 'normal'),
+('rayenn kahammar', crypt('pass10', gen_salt('bf')), 'normal');
