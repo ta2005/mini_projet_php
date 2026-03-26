2d138020c497a234804f7e74d052b3138767f967
Souhail-Bel <robotboy976@gmail.com>
2026-03-26 21:43:22 +0100

    Switched to dynamic landing page

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Login Page</title>
     <link href="style.css" rel="stylesheet" />
   </head>
   <body>
+	<!--   Display error message if error is set from login.php -->
+	<?php if (isset($_GET['error'])): ?>
+		<div id="error-msg">
+			Vérifier vos crédentiels!
+			<span class="close-btn" onclick="document.getElementById('error-msg').style.display='none'">&times;</span>
+		</div>
+	<?php endif; ?>
+
+
     <form method="POST" action="login.php">
       <label for="user_name">
         <p>Username</p>
