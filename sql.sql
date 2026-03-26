f534dcf1c348e344c6dde2438714655fc4efbbbd
Talel Zighni <talelzigni24@gmail.com>
2026-03-26 16:59:21 +0100

    added basic sql and html

---
+++ sql.sql
@@ -0,0 +1,35 @@
+-- CREATE TYPE  role AS  ENUM ('normal','admin');
+
+CREATE TABLE IF NOT EXISTS utilisateur(
+    id SERIAL,
+    name VARCHAR(100) NOT NULL,
+    --5allehe 3allah
+    password VARCHAR(100) NOT NULL,
+    role role,
+    CONSTRAINT pk_user PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS section(
+    id SERIAL PRIMARY KEY,
+    des TEXT
+);
+
+CREATE TABLE IF NOT EXISTS etudiant(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    date_de_naiss DATE NOT NULL,
+    img VARCHAR(256) NOT NULL,
+    section INTEGER,
+    CONSTRAINT fk_section FOREIGN KEY(section) REFERENCES SECTION(id)
+);
+
+
+-- INSERT INTO  utilisateur(name,password,role) VALUES
+-- ('talel zighni','123','admin'),
+-- ('ahmed el hai','1234','normal');
+
+-- Un user normal n’a le droit qu’à la consultation
+-- 7. Vous allez avoir une table etudiant avec un nom, une
+-- image, une date de naissance et une section
+-- 8. La table section devra avoir une designation et une
+-- description.
