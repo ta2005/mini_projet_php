d040be0762aefeabcc3e1605b26548da5abdfeab
Rayen Khammar <khammarrayen4@gmail.com>
2026-03-26 21:23:50 +0100

    added  dark theme

--- index.php
+++ index.php
@@ -1,58 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<style>
-	*, *::before, *::after {
-	    /* This stops padding from making boxes wider than you set them */
-	    margin: 0;
-	    padding: 0;
-	}
-	
-	body{
-	    display:flex;
-	    justify-content:center;
-	    align-items:center;
-	}
+<?php $mode = $_COOKIE['mode'] ?? 'dark'; ?>
+<html data-bs-theme="dark" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-	form{
-	    margin:0px auto;
-	}
+<body>
+    <div class="container" >
+    <form action="processLogin.php" method="POST">
+        <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="username" name="user" class="form-control" id="username">
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    </div>
+</body>
 
-	input{
-		font-size:16px;
-	    margin:8px 0;
-	}
-	.note{
-	    font-size:14px;
-	    margin-top:-8px;
-	    margin-bottom:8px;
-	}
-
-	button{
-	    padding:5px;
-	    background-color:blue;
-	    border-radius:5px;
-	    color:white;
-	}
-	</style>
-    </head>
-    <body>
-        <?php setcookie('mode','light'); ?>
-	<form action="processLogin" method="POST" >
-	    <label for="user_name">  
-		<p>username</p>
-		<input type="text" value="" id="user_name" placeholder="Enter Username">
-		<p class="note">never share you user name</p>
-	    </label>
-	    <label for="pwd">
-		<p>Password</p>
-		<input type="password" value="" id="pwd" placeholder="Password">
-		<p><button type="submit">Login</button></p>
-		<label>
-
-	</form>
-    </body>
 </html>
