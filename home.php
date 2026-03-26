c33d4450d8c0494ad34704c40db70a1ab0aa3d31
Rayen Khammar <khammarrayen4@gmail.com>
2026-03-26 23:39:01 +0100

    added the pages

---
+++ home.php
@@ -0,0 +1,27 @@
+<?php session_start(); 
+if (!isset($_SESSION['user']) || !isset($_SESSION['role'])) {
+    header("Location: index.php");
+    exit();}
+?>
+<!DOCTYPE html>  
+<?php $mode = $_COOKIE['mode'] ?? 'dark'; ?>
+<html data-bs-theme="dark" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container" >
+    <p> Hello, PHP Lovers! Welcome to your administration Platform </p>
+    <?php if($_SESSION['role'] == 'admin') : ?>
+        <a href="admin.php" class="btn btn-primary">Students List</a>
+    <?php else : ?>
+        <a href="normal.php" class="btn btn-primary">Students List</a>
+    <?php endif; ?>
+    </div>
+</body>
+
+</html>
--- processLogin.php
+++ processLogin.php
@@ -5,25 +5,41 @@ $password = $_POST["password"];
 
 $bdd=ConnexionBD::getInstance();
 $query="SELECT * FROM users WHERE username=? AND password=?";
-$response=$bdd->query($query);
+$response=$bdd->prepare($query);
 $response->execute([$user,$password]);
 $result=$response->fetch();
-if(!isset($result)) {
+if(!$result) {
     echo "عاود ثبت";
 }
 else {
-    $query="SELECT role from users where username=$user AND password=$password ";
-    $response=$bdd->query($query);
-    $response->execute();
-    $result=$response->fetch();
-    if($resutlt='admin') {
-        $_SESSION['user']=$user;
+    $role = $result['role'];
+    $_SESSION['user']=$user;
+    $_SESSION['role']=$result;
+
+    if($role='admin') {
         header('location:admin.php');
     }
     else {
-        $_SESSION['user']=$user;
         header('location:normal.php');
     }
+    exit();
 }
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+<?php if ($error): ?>
+<div class="alert alert-danger alert-dismissible fade show" style="width: 250px;">
+    <strong>Login</strong>
+    <button type="button" class="btn-close float-end" data-bs-dismiss="alert"></button>
+    <br>
+    <small><?php echo $error; ?></small>
+</div>
+<?php endif; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
